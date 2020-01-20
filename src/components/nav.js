ba19f1ebc4224f9e645b225803f85d99f0dd7f97
Peter Durham <peterdurham@protonmail.com>
2020-01-20 13:39:12 -0800

    styled nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { FiMenu } from "react-icons/fi"
+import { IoMdSearch } from "react-icons/io"
+
 const Nav = () => {
   return (
-    <nav>
-      <div>Menu Button</div>
-      <div>
-        <Link to="/">Code Boost Logo</Link>
-        <p>None of that nonsense, all of that good stuff</p>
+    <nav className="Nav">
+      <button className="Nav__menu--button">
+        <FiMenu />
+        <span>Menu</span>
+      </button>
+      <div className="Nav__logo">
+        <Link to="/">
+          <span>Code Boost Logo</span>
+          <p>None of that nonsense, all of that good stuff</p>
+        </Link>
       </div>
-      <div>Search</div>
+      <button className="Nav__search--button">
+        <IoMdSearch />
+      </button>
     </nav>
   )
 }
