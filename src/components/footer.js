103ab2e0a2d19969e1087679c1fd3d822db7fc83
Peter Durham <peterdurham@protonmail.com>
2020-01-20 09:49:44 -0800

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,59 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { AiOutlineInstagram } from "react-icons/ai"
+import {
+  TiSocialFacebook,
+  TiSocialTwitter,
+  TiSocialYoutube,
+} from "react-icons/ti"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allTopicsJson {
+        edges {
+          node {
+            name
+            slug
+          }
+        }
+      }
+    }
+  `)
+  let topics = data.allTopicsJson.edges
   return (
-    <footer>
-      <div>Code Boost</div>
-      <div>
-        <div>social here</div>
-        <div>Links here</div>
+    <footer className="Footer">
+      <div className="Footer__logo">Code Boost</div>
+      <div className="Footer__content">
+        <div className="Footer__social">
+          <div className="Footer__social--text">Follow Us</div>
+          <div className="Footer__social--icons">
+            <span className="Footer__social--icon-container" id="tw-icon">
+              <TiSocialTwitter className="Footer__social--icon" />
+            </span>
+            <span className="Footer__social--icon-container" id="yt-icon">
+              <TiSocialYoutube className="Footer__social--icon" />
+            </span>
+            <span className="Footer__social--icon-container" id="ig-icon">
+              <AiOutlineInstagram className="Footer__social--icon" />
+            </span>
+            <span className="Footer__social--icon-container" id="fb-icon">
+              <TiSocialFacebook className="Footer__social--icon" />
+            </span>
+          </div>
+        </div>
+        <div className="Footer__links">
+          {topics.map(({ node: item }) => {
+            return <div className="Footer__link">{item.name}</div>
+          })}
+        </div>
       </div>
-      <div>
+      <div className="Footer__copyright">
         {" "}
         © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        Gatsby
       </div>
     </footer>
   )
