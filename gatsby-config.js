bc9ebf62988c4cb7ef559c570b8bc939d04a40c7
Peter Durham <peterdurham@protonmail.com>
2020-04-25 08:28:58 -0700

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-137973684-4",
       },
     },
     {
