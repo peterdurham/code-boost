df7ba2c7c999705fb1ff65b1fe55f698d1cce89e
Peter Durham <peterdurham@protonmail.com>
2020-05-17 22:43:20 -0700

    adjusted node file

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,9 +5,9 @@ const { fmImagesToRelative } = require("gatsby-remark-relative-images")
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  fmImagesToRelative(node)
 
   if (node.internal.type === `MarkdownRemark`) {
+    fmImagesToRelative(node)
     const value = createFilePath({ node, getNode })
 
     createNodeField({
