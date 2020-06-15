01f47c513c689b5732e2ac6ec0a67319fc3f5ac6
Peter Durham <peterdurham@protonmail.com>
2020-06-15 11:16:51 -0700

    added full stack image

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,131 +3,6 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const getSchemaOrgJSONLD = ({
-  isBlogPost,
-  url,
-  title,
-  image,
-  description,
-  datePublished,
-  dateModified,
-  tags,
-  postImage,
-}) => {
-  const schemaOrgJSONLD = [
-    {
-      "@context": "http://schema.org",
-      "@type": "WebSite",
-      "@id": "https://code-boost.com#website",
-      url: "https://code-boost.com",
-      name: "Code-Boost",
-      alternateName: "code-boost",
-      author: {
-        "@id": "https://code-boost.com#organization",
-      },
-    },
-  ]
-
-  return isBlogPost
-    ? [
-        ...schemaOrgJSONLD,
-        {
-          "@context": "http://schema.org",
-          "@type": "WebPage",
-          "@id": `${url}#blog-post`,
-          url,
-          headline: title,
-          description: description,
-          publisher: {
-            "@id": "https://code-boost.com/#organization",
-          },
-          datePublished,
-          dateModified,
-          breadcrumb: {
-            "@type": "BreadcrumbList",
-            itemListElement: [
-              {
-                "@type": "ListItem",
-                position: 1,
-                item: {
-                  "@id": "https://code-boost.com",
-                  name: "Home",
-                },
-              },
-
-              {
-                "@type": "ListItem",
-                position: 2,
-                item: {
-                  "@id": "https://code-boost.com",
-                  name: "Tutorials",
-                },
-              },
-              {
-                "@type": "ListItem",
-                position: 3,
-                item: {
-                  "@id": url,
-                  name: title,
-                },
-              },
-            ],
-          },
-          sourceOrganization: {
-            "@id": "https://code-boost.com/#organization",
-          },
-        },
-        {
-          "@context": "http://schema.org",
-          "@type": "TechArticle",
-          "@id": `${url}#blog-post`,
-          mainEntityOfPage: {
-            "@type": "WebPage",
-            "@id": url,
-          },
-          url: url,
-          headline: title,
-          // wordCount: null,
-          description: description,
-          audience: "JavaScript developers",
-
-          image: {
-            "@type": "ImageObject",
-            url: postImage,
-            height: 314,
-            width: 600,
-          },
-
-          keywords: tags,
-          datePublished,
-          dateModified,
-          articleSection: "Tutorials",
-          author: {
-            "@type": "Person",
-            name: "Peter Durham",
-
-            image: {
-              "@type": "ImageObject",
-              url: image,
-              height: 300,
-              width: 300,
-            },
-
-            url: "https://peterdurham.netlify.com/",
-          },
-
-          publisher: {
-            name: "Code Boost",
-            url: "https://code-boost.com/",
-            logo:
-              "https://pbs.twimg.com/profile_images/1047970722646245380/buKQBtWY_400x400.jpg",
-            "@id": `${url}#organization`,
-          },
-        },
-      ]
-    : schemaOrgJSONLD
-}
-
 function SEO({ description, title, slug, frontmatter, isBlogPost, canonical }) {
   const { site, file, logo } = useStaticQuery(
     graphql`
@@ -153,6 +28,130 @@ function SEO({ description, title, slug, frontmatter, isBlogPost, canonical }) {
     `
   )
 
+  const getSchemaOrgJSONLD = ({
+    isBlogPost,
+    url,
+    title,
+    image,
+    description,
+    datePublished,
+    dateModified,
+    tags,
+    postImage,
+  }) => {
+    const schemaOrgJSONLD = [
+      {
+        "@context": "http://schema.org",
+        "@type": "WebSite",
+        "@id": "https://code-boost.com#website",
+        url: "https://code-boost.com",
+        name: "Code-Boost",
+        alternateName: "code-boost",
+        author: {
+          "@id": "https://code-boost.com#organization",
+        },
+      },
+    ]
+
+    return isBlogPost
+      ? [
+          ...schemaOrgJSONLD,
+          {
+            "@context": "http://schema.org",
+            "@type": "WebPage",
+            "@id": `${url}#blog-post`,
+            url,
+            headline: title,
+            description: description,
+            publisher: {
+              "@id": "https://code-boost.com/#organization",
+            },
+            datePublished,
+            dateModified,
+            breadcrumb: {
+              "@type": "BreadcrumbList",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  item: {
+                    "@id": "https://code-boost.com",
+                    name: "Home",
+                  },
+                },
+
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  item: {
+                    "@id": "https://code-boost.com",
+                    name: "Tutorials",
+                  },
+                },
+                {
+                  "@type": "ListItem",
+                  position: 3,
+                  item: {
+                    "@id": url,
+                    name: title,
+                  },
+                },
+              ],
+            },
+            sourceOrganization: {
+              "@id": "https://code-boost.com/#organization",
+            },
+          },
+          {
+            "@context": "http://schema.org",
+            "@type": "TechArticle",
+            "@id": `${url}#blog-post`,
+            mainEntityOfPage: {
+              "@type": "WebPage",
+              "@id": url,
+            },
+            url: url,
+            headline: title,
+            // wordCount: null,
+            description: description,
+            audience: "JavaScript developers",
+
+            image: {
+              "@type": "ImageObject",
+              url: postImage,
+              height: 314,
+              width: 600,
+            },
+
+            keywords: tags,
+            datePublished,
+            dateModified,
+            articleSection: "Tutorials",
+            author: {
+              "@type": "Person",
+              name: "Peter Durham",
+
+              image: {
+                "@type": "ImageObject",
+                url: image,
+                height: 300,
+                width: 300,
+              },
+
+              url: "https://peterdurham.netlify.com/",
+            },
+
+            publisher: {
+              name: "Code Boost",
+              url: "https://code-boost.com/",
+              logo:
+                "https://pbs.twimg.com/profile_images/1047970722646245380/buKQBtWY_400x400.jpg",
+              "@id": `${url}#organization`,
+            },
+          },
+        ]
+      : schemaOrgJSONLD
+  }
   const metaDescription = description || site.siteMetadata.description
   const postImage = isBlogPost
     ? `https://code-boost.com${frontmatter.featuredImage.childImageSharp.fluid.src}`
@@ -169,7 +168,6 @@ function SEO({ description, title, slug, frontmatter, isBlogPost, canonical }) {
     datePublished: isBlogPost ? frontmatter.date : "",
     dateModified: isBlogPost ? frontmatter.dateModified : "",
   })
-
   return (
     <Helmet
       htmlAttributes={{
